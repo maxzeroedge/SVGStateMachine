84403e2d7d1a27765f4f1e5778da516949b132e6
Palash <maxzeroedge@gmail.com>
2021-03-21 22:25:17 +0530

    added tokenizer with size calculation

--- tokenizer.js
+++ tokenizer.js
@@ -1,34 +1,63 @@
 var TextTokenizer = {
     getWordDelimiters: [
-        " ", "\n", "\t", ".", ",", "?", ":", ";"
+        " ", "\n", "\t", ".", ",", "?", ":", ";", "!", "-", "/"
     ],
-    getWords(fullText){
+    getWords({fullText}){
         const wordTokens = [];
         if(fullText){
-            let currentWord = ""
+            let currentWord = "";
             let delimiter = "";
             fullText.split("").forEach((currentLetter) => {
                 if(TextTokenizer.getWordDelimiters.indexOf(currentLetter) > -1){
                     delimiter += currentLetter
                     if(currentWord) {
-                        wordTokens.push(currentWord);
+                        wordTokens.push({currentWord});
                         currentWord = ""
                     }
                 } else {
                     if(delimiter){
-                        wordTokens.push(delimiter);
+                        wordTokens.push({
+                            currentWord: delimiter
+                        });
                         delimiter = ""
                     }
                     currentWord += currentLetter;
                 }
             })
             if(currentWord){
-                wordTokens.push(currentWord);
+                wordTokens.push({currentWord});
             } else if(delimiter) {
-                wordTokens.push(delimiter);
+                wordTokens.push({
+                    currentWord: delimiter
+                });
             }
         }
         return wordTokens;
+    },
+    getWordWidth(attrs) {
+        const {
+            currentWord, fontFamily, fontSize, isBold, isItalic
+        } = attrs;
+        const canvas = document.getElementById("text-tokenizer-canvas");
+        const context = canvas.getContext("2d");
+        let font = "";
+        if(isBold){
+            font += "bold ";
+        }
+        if(isItalic) {
+            font += "italic ";
+        }
+        if(fontSize) {
+            font += fontSize;
+        }
+        if(fontFamily) {
+            font += fontFamily;
+        }
+        context.font = font;
+        const metrics = context.measureText(currentWord);
+        attrs.width = Math.round(metrics.width);
+        attrs.height = Math.round(metrics.height);
+        return attrs;
     }
 }
 
